3c493ffe531b7df72b5439c6a69e0987ab8655db
MironAlberto <alberto.miron19@gmail.com>
2024-01-18 09:51:30 +0100

    esercizio completato

---
+++ vite-project/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+    import AppTitle from './components/AppTitle.vue';
+    export default {
+        data() {
+            return {  
+            };
+        }, 
+        components: {
+            AppTitle
+        }
+    };
+</script>
+
+<template>
+    <div>
+        <AppTitle/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
